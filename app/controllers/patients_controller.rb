042fc638c5222d9b9e8e503a39db87f0bb322c2f
Chloe Bradley <ccbradley77@gmail.com>
2015-03-10 13:38:44 -0600

    created delete patient and delete medication/dependent destroy

--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -2,4 +2,10 @@ class PatientsController < ApplicationController
   def show
     @patient = Patient.find(params[:id])
   end
+
+  def destroy
+    patient = Patient.find(params[:id])
+    patient.destroy
+    redirect_to root_path, notice: " Patient was successfully deleted"
+end
 end
