5c33250532295f43b96cdeb8ce6f4de976af0530
Tristan Rubadeau <tristan@rubadeau.com>
2015-03-10 13:38:13 -0600

    all the things

--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -2,4 +2,11 @@ class PatientsController < ApplicationController
   def show
     @patient = Patient.find(params[:id])
   end
+
+  def destroy
+    patient = Patient.find(params[:id])
+    patient.destroy
+    redirect_to root_path, notice: "Gone"
+  end
+
 end
