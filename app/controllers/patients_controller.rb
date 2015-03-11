89a3eda8898e6e073a7bba4c14dfcd9dda32818a
Annie Lydens <aelydens@gmail.com>
2015-03-10 20:16:43 -0600

    Add destroy action and links to Patients and Prescriptions

--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -2,4 +2,11 @@ class PatientsController < ApplicationController
   def show
     @patient = Patient.find(params[:id])
   end
+
+  def destroy
+    patient = Patient.find(params[:id])
+    patient.destroy
+    flash[:notice] = "Patient and prescriptions successfully removed"
+    redirect_to root_path
+  end
 end
